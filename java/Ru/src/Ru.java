4d0834a00f6fb63797cb40bedf2111b5675bfe44
Tomás Abril <tomasabril@users.noreply.github.com>
2015-10-20 19:49:03 -0200

    Criar classes

---
+++ java/Ru/src/Ru.java
@@ -0,0 +1,21 @@
+
+import Interface.*;
+import BandodeDados.*;
+import ru.*;
+
+/**
+ *
+ * @author Tomás Abril
+ */
+
+public class Ru {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Janela().setVisible(true);
+    }
+    
+}
